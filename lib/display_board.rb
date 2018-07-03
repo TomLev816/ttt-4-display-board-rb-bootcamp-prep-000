2bbaeaf50cb2719298e259c9fa6903dadbe800f8
Thomas Lever <thomaslever508@gmail.com>
2018-07-03 18:02:20 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,6 +2,8 @@
 # out the current state.
 def display_board(x)
   puts " #{x[0]} | #{x[1]} | #{x[2]} "
+  puts "-----------"
   puts " #{x[3]} | #{x[4]} | #{x[5]} "
+  puts "-----------"
   puts " #{x[6]} | #{x[7]} | #{x[8]} "
 end
